de01bffe89ab21eab9934ec96a89a202f1f472e7
Michael Park <migrap@gmail.com>
2013-04-24 20:35:26 -0700

    Expose setters to Newtonsoft

--- src/Bitstamp/Bitstamp/Models/Ticker.cs
+++ src/Bitstamp/Bitstamp/Models/Ticker.cs
@@ -2,27 +2,27 @@ using System;
 using Newtonsoft.Json;
 
 namespace Bitstamp {
-	public class Ticker {
-		[JsonConverter(typeof(StringToDoubleConverter))]
-		public double Bid{ get; internal set;}
+    public class Ticker {
+        [JsonConverter(typeof(StringToDoubleConverter))]
+        public double Bid { get; set; }
 
-		[JsonConverter(typeof(StringToDoubleConverter))]
-		public double Ask{ get; internal set;}
+        [JsonConverter(typeof(StringToDoubleConverter))]
+        public double Ask { get; set; }
 
-		[JsonConverter(typeof(StringToDoubleConverter))]
-		public double Last{get;internal set;}
+        [JsonConverter(typeof(StringToDoubleConverter))]
+        public double Last { get; set; }
 
-		[JsonConverter(typeof(StringToDoubleConverter))]
-		public double High{get;internal set;}
+        [JsonConverter(typeof(StringToDoubleConverter))]
+        public double High { get; set; }
 
-		[JsonConverter(typeof(StringToDoubleConverter))]
-		public double Low{get;internal set;}
+        [JsonConverter(typeof(StringToDoubleConverter))]
+        public double Low { get; set; }
 
-		[JsonConverter(typeof(StringToDoubleConverter))]
-		public double Volume{get;internal set;}
+        [JsonConverter(typeof(StringToDoubleConverter))]
+        public double Volume { get; set; }
 
-		public override string ToString () {
-			return string.Format ("[Ticker: Bid={0}, Ask={1}, Last={2}, High={3}, Low={4}, Volume={5}]", Bid, Ask, Last, High, Low, Volume);
-		}
-	}
+        public override string ToString() {
+            return string.Format("[Ticker: Bid={0}, Ask={1}, Last={2}, High={3}, Low={4}, Volume={5}]", Bid, Ask, Last, High, Low, Volume);
+        }
+    }
 }
