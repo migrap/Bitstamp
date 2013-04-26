2191fb45f5bcb117d0f95caf58508c84c3dfe6be
Michael Park <migrap@gmail.com>
2013-04-25 21:09:36 -0700

    Adding aggreate to levels

--- src/Bitstamp/Bitstamp/Models/OrderBook.cs
+++ src/Bitstamp/Bitstamp/Models/OrderBook.cs
@@ -19,8 +19,82 @@ namespace Bitstamp {
 	public class Level {
 		public double Price{get;set;}
 		public double Volume{get;set;}
+		public double Value{ get { return Price * Volume; } }
+		public Aggregate Aggregate{ get; set; }
 	}
 
-	public class Levels:List<Level>{
+	public class Levels:ICollection<Level>{
+		private List<Level> _levels = new List<Level> ();
+
+		public Level this[int index]{
+			get{ return _levels [index];}
+		}
+
+		public void AddRange(IEnumerable<Level> collection){
+			foreach (var item in collection) {
+				Add (item);
+			}
+		}
+
+		public void Add (Level item) {
+			item.Aggregate = item;
+			if (Count != 0) {
+				item.Aggregate += _levels [Count - 1].Aggregate;
+			}
+			_levels.Add (item);
+		}
+
+		public void Clear () {
+			_levels.Clear ();
+		}
+
+		public bool Contains (Level item) {
+			return _levels.Contains (item);
+		}
+
+		public void CopyTo (Level[] array, int arrayIndex) {
+			_levels.CopyTo (array, arrayIndex);
+		}
+
+		public bool Remove (Level item) {
+			return _levels.Remove (item);
+		}
+
+		public int Count {
+			get{ return _levels.Count;}
+		}
+
+		public bool IsReadOnly {
+			get{ return false;}
+		}
+
+		public IEnumerator<Level> GetEnumerator () {
+			return _levels.GetEnumerator ();
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator () {
+			return _levels.GetEnumerator ();
+		}
+	}
+
+	public class Aggregate{
+		public double Price{get;set;}
+		public double Volume{get;set;}
+		public double Value{ get; set; }
+
+		public static implicit operator Aggregate(Level value){
+			return new Aggregate {
+				Price=value.Price,
+				Volume=value.Volume,
+				Value=value.Value,
+			};
+		}
+
+		public static Aggregate operator +(Aggregate lhs, Aggregate rhs){
+			lhs.Volume += rhs.Volume;
+			lhs.Value += rhs.Value;
+			lhs.Price = (lhs.Volume / lhs.Value);
+			return lhs;
+		}
 	}
 }
