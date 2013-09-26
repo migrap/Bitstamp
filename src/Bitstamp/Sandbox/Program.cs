0340fd57c9878f383a927ae7f838127f87d6031c
Michael Park <migrap@gmail.com>
2013-09-26 14:29:27 -0700

    Jay Jay

--- src/Bitstamp/Sandbox/Program.cs
+++ src/Bitstamp/Sandbox/Program.cs
@@ -1,21 +1,39 @@
 using System;
-using Bitstamp;
 using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using SocketIOClient;
+using PusherClient;
 
 namespace Sandbox {
 	class MainClass {
 		public static void Main (string[] args) {
-			var bitstamp = new BitstampClient ();
-			var ticker = bitstamp.GetTickeAsync().Result;
-			var orderbook = bitstamp.GetOrderBookAsync ().Result;
+            var pusher = new Pusher("de504dc5763aeef9ff52");
+            var channel = (Channel)null;
+            pusher.Connected += (s) => {
+                Console.WriteLine("Connected");
+                channel = pusher.Subscribe("live_orders");
+                channel.Subscribed += channel_Subscribed;
+                
+            };
+            pusher.ConnectionStateChanged += (s, cs) => Console.WriteLine(cs);
 
-			Console.WriteLine (ticker);
+            pusher.Connect();
 
-			(new AutoResetEvent (false)).WaitOne ();
+            Console.ReadLine();
+
+			(new AutoResetEvent (false)).WaitOne();
 		}
+
+        static void channel_Subscribed(object sender) {
+            var channel = sender as Channel;
+            channel.Bind("order_changed", (d) => {
+                Console.WriteLine("order_changed: {0}", d.ToString());
+            });
+        }
 	}
 }
